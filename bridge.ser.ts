452cf7f94b024631bab5c8e46b702c978afe6abe
Bolza <stefano.sergio@gmail.com>
2016-03-17 16:44:51 +0000

    first commit

---
+++ bridge.ser.ts
@@ -0,0 +1,20 @@
+import { Injectable } from 'angular2/core';
+import { Subject } from 'rxjs';
+
+let eventMap = {}
+
+@Injectable()
+export class Bridge {
+    on(to, cb) {
+    	if (!eventMap[to]) {
+    		eventMap[to] = new Subject()
+    	}
+    	eventMap[to].subscribe(cb);
+    }
+
+    save(key, value) {
+    	if (eventMap[key]) {
+    		eventMap[key].next(value)
+    	}
+    }
+}
