1face5668b01fd73da77ed9bf1318501c1fbd1ee
Bolza <stefano.sergio@gmail.com>
2016-03-17 16:52:33 +0000

    adding the ng2 example directive

---
+++ src/ng2/ng2component.ser.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from 'angular2/core';
+import {Bridge} from 'components/bridge/bridge.ser';
+let theBridge 
+
+@Component({
+    selector: 'some-component',
+    template: `<div [model]="'eventName'" (click)="select('someValue')"></div>`
+})
+export class someComponent {
+    @Input() model;
+    
+    constructor() {
+        theBridge = new Bridge()
+    }
+    select(value) {
+        theBridge.save(this.model, value)
+    }
+}
